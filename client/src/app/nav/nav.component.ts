30d85484bf3eaee0c80431114e2f981e391efd57
ahmadalkhatami <ahmadalkhatami@gmail.com>
2023-03-15 22:56:42 +0700

    End of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -10,20 +12,24 @@ import { User } from '../_models/user';
 })
 export class NavComponent implements OnInit {
   model: any = {};
+user: any;
 
-  constructor(public accountService: AccountService) {}
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {}
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: (response) => {
-        console.log(response);
-      },
-      error: (error) => console.log(error),
+      next: (_) => this.router.navigateByUrl('/members'), //redirect ke /members ketika login
+      error: (error) => this.toastr.error(error.error), //error alert dengan toastr
     });
   }
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/'); //redirect ke home page ketika logout
   }
 }
